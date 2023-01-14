53ec2004911c53862e12f95efab465dbd9b8e135
Dmitrii Gienko <cuanik@mail.ru>
2023-01-14 18:34:37 +0300

    В процессе...

--- src/Employee.java
+++ src/Employee.java
@@ -1,2 +1,54 @@
 public class Employee {
+    private String fullName;
+    private int dep;
+    private double salary;
+    private int id;
+    public static int count = 0;
+
+
+    public Employee(String fullName, int dep, int salary) {
+        this.fullName = fullName;
+        this.dep = dep;
+        this.salary = salary;
+        count += 1;
+        this.id = count;
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public int getDep() {
+        return dep;
+    }
+
+    public void setDep(int dep) {
+        this.dep = dep;
+    }
+
+    @Override
+    public String toString() {
+        return "Сотрудник: " + fullName + "\n" +
+                "..........отдел: " + dep + "\n" +
+                "..........зарплата: " + salary + "\n" +
+                "..........Id: " + getId()+ "\n" + "=========================================";
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,71 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Employee[] employees = new Employee[10];
+        employees[0] = new Employee("Филимонова Ольга Павловна", 1, 58500);
+        employees[1] = new Employee("Вильгельмс Валерий Борисович", 2, 52000);
+        employees[2] = new Employee("Фаткулин Ринат Исламович", 4, 57300);
+        employees[3] = new Employee("Акимочкина Яна Юрьевна", 3, 53100);
+        employees[4] = new Employee("Петросян Дмитрий Владимироваич", 3, 52500);
+        employees[5] = new Employee("Торопина Ольга Владимировна", 2, 55200);
+        employees[6] = new Employee("Гастренко Александра Ивановна", 4, 54200);
+        employees[7] = new Employee("Горбунова Алена Дмитриевна", 5, 59200);
+        employees[8] = new Employee("Яркова Екатерина Дмитриевна", 2, 59100);
+        employees[9] = new Employee("Шомысова Наталья Ивановна", 5, 56200);
+
+// Печать всех сотрудников
+
+
+
+//        for (Employee strings : employees) {
+//            System.out.println(strings.toString());
+//        }
+
+// сумма зарплаты
+        double sum = 0;
+        for (int i = 0; i < employees.length; i++) {
+            sum += employees[i].getSalary();
+        }
+        System.out.println("Cуммa затрат на зарплаты в месяц составляет: " + sum + " рублей.");
+
+// минимальная зарплата
+        double minSalary = employees[0].getSalary();
+        String employeeMinSal = null;
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i].getSalary() < minSalary) {
+                minSalary = employees[i].getSalary();
+                employeeMinSal = employees[i].getFullName();
+            }
+        }
+        System.out.println("Сотрудник " + employeeMinSal + " получает минимальную зарплату " + minSalary + " рублей");
+
+// максимальная зарплата
+        double maxSalary = employees[0].getSalary();
+        String employeeMaxSal = null;
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i].getSalary() > maxSalary) {
+                maxSalary = employees[i].getSalary();
+                employeeMaxSal = employees[i].getFullName();
+            }
+        }
+        System.out.println("Сотрудник " + employeeMaxSal + " получает максимальную зарплату " + maxSalary + " рублей");
+
+// средняя зарплата
+        System.out.println("Средняя зарплата составляет " + sum / employees.length);
+
+        for (Employee names : employees) {
+            System.out.println(names.getFullName());
+        }
+
     }
+
+    public  void printAllEmployee() {
+        for (Employee strings : new Employee) {
+            System.out.println(strings.toString());
+        }
+
+    }
+
+
+
+
 }
