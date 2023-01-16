89c17cefebb6114694a93467ecb3dd6372066be3
Dmitrii Gienko <cuanik@mail.ru>
2023-01-16 13:33:38 +0300

    Базовая сложность готова

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        PersonelManagement  staff = new PersonelManagement();
+        PersonelManagement staff = new PersonelManagement();
         staff.addEmployee("Филимонова Ольга Павловна", 1, 58500);
         staff.addEmployee("Вильгельмс Валерий Борисович", 2, 52000);
         staff.addEmployee("Фаткулин Ринат Исламович", 4, 57300);
@@ -12,56 +12,18 @@ public class Main {
         staff.addEmployee("Яркова Екатерина Дмитриевна", 2, 59100);
         staff.addEmployee("Шомысова Наталья Ивановна", 5, 56200);
 
-       staff.printAllEmployees();
+        staff.printAllEmployees();
 
+        staff.printTotalPay();
 
-//        for (Employee strings : employees) {
-//            System.out.println(strings.toString());
-//        }
+        staff.printEmpMinSal();
 
-// сумма зарплаты
-        /*double sum = 0;
-        for (int i = 0; i < employees.length; i++) {
-            sum += employees[i].getSalary();
-        }
-        System.out.println("Cуммa затрат на зарплаты в месяц составляет: " + sum + " рублей.");
+        staff.printEmpMaxSal();
 
-// минимальная зарплата
-        double minSalary = employees[0].getSalary();
-        String employeeMinSal = null;
-        for (int i = 0; i < employees.length; i++) {
-            if (employees[i].getSalary() < minSalary) {
-                minSalary = employees[i].getSalary();
-                employeeMinSal = employees[i].getFullName();
-            }
-        }
-        System.out.println("Сотрудник " + employeeMinSal + " получает минимальную зарплату " + minSalary + " рублей");
+        staff.printAverageSalary();
 
-// максимальная зарплата
-        double maxSalary = employees[0].getSalary();
-        String employeeMaxSal = null;
-        for (int i = 0; i < employees.length; i++) {
-            if (employees[i].getSalary() > maxSalary) {
-                maxSalary = employees[i].getSalary();
-                employeeMaxSal = employees[i].getFullName();
-            }
-        }
-        System.out.println("Сотрудник " + employeeMaxSal + " получает максимальную зарплату " + maxSalary + " рублей");
+        staff.printFullNames();
 
-// средняя зарплата
-        System.out.println("Средняя зарплата составляет " + sum / employees.length);
-
-        for (Employee names : employees) {
-            System.out.println(names.getFullName());
-        }
-
-    }
-
-    public  void printAllEmployee() {
-        for (Employee strings : new Employee) {
-            System.out.println(strings.toString());
-        }
-*/
     }
 
 
--- src/PersonelManagement.java
+++ src/PersonelManagement.java
@@ -1,16 +1,10 @@
 public class PersonelManagement {
-
     private final Employee[] employee;
-
     private int numberOfEmployees;
 
     public PersonelManagement() {
         this.employee = new Employee[10];
     }
-
-   /* public int getId(int id) {
-        return this.id=id;
-    }*/
 
     public void addEmployee(String fullName, int dep, double salary) {
         Employee newEmployee = new Employee(fullName, dep, salary);
@@ -22,6 +16,58 @@ public class PersonelManagement {
     public void printAllEmployees() {
         for (int i = 0; i < employee.length; i++) {
             System.out.println(employee[i].toString());
+        }
+    }
+
+    // сумма затрат на зарплату
+    public void printTotalPay() {
+        double TotalSalary = 0;
+        for (int i = 0; i < employee.length; i++) {
+            TotalSalary += employee[i].getSalary();
+        }
+        System.out.printf("\nСумма затрат на зарплаты сотрудников %.2f руб.\n", TotalSalary);
+    }
+
+    //сотрудник с минимальной зарплатой
+    public void printEmpMinSal() {
+        double minSalary = employee[0].getSalary();
+        String employeeMinSal = null;
+        for (int i = 0; i < employee.length; i++) {
+            if (employee[i].getSalary() < minSalary) {
+                minSalary = employee[i].getSalary();
+                employeeMinSal = employee[i].getFullName();
+            }
+        }
+        System.out.printf("\nCотрудник с минимальной зарплатой в %.2f руб.:\t%s\n", minSalary, employeeMinSal);
+    }
+
+    //сотрудник с максимальной зарплатой
+    public void printEmpMaxSal() {
+        double maxSalary = employee[0].getSalary();
+        String employeeMaxSal = null;
+        for (int i = 0; i < employee.length; i++) {
+            if (employee[i].getSalary() > maxSalary) {
+                maxSalary = employee[i].getSalary();
+                employeeMaxSal = employee[i].getFullName();
+            }
+        }
+        System.out.printf("\nCотрудник с максимальной зарплатой в %.2f руб.:\t%s\n", maxSalary, employeeMaxSal);
+    }
+
+    //расчет средней зарплаты
+    public void printAverageSalary() {
+        double TotalSalary = 0;
+        for (int i = 0; i < employee.length; i++) {
+            TotalSalary += employee[i].getSalary();
+        }
+        System.out.printf("\nСредняя зарплата составляет %.2f руб.\n", TotalSalary / numberOfEmployees);
+    }
+
+    //вывод ФИО сотрудников
+    public void printFullNames() {
+        System.out.println("\nСписок сотрудников:");
+        for (int i = 0; i < employee.length; i++) {
+            System.out.println(employee[i].getFullName());
         }
     }
 
